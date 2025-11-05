8280b06c376f4eb785d66437362c6837b2ec5e4d
HyagoRM <hyago.molena@escola.pr.gov.br>
2025-11-05 16:59:52 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,3 +1,7 @@
 #colocar titulo e descrever o jogo
 print("Jogo de perguntas")
+
 print("o jogo será trabalhado ultilizando formas esternas para desenvolver respostas e novas perguntas")
+#pensar em um tema
+nome = input("Qual o seu nome?")
+print("olá", nome)
